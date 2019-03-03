799b9ec2a10fc5a85c793ca6466bfa9965bce637
Kumar Kartikay <krkartikay@gmail.com>
2019-03-04 00:02:24 +0530

    correct test

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,6 +6,7 @@ def test_particle_data():
     u.add(p)
     p = physics.Particle((1, 2, 3), (2, -1, 2), 4)
     u.add(p)
-    expected = {'particles': [{'mass': 1, 'pos': [0, 0, 0], 'vel': 
-                [1, 1, 1]}, {'mass': 4, 'pos': [1, 2, 3], 'vel': [2, -1, 2]}]}
-    assert u.data() == expected
+    expected = [{'mass': 1, 'pos': [0, 0, 0], 'vel': [1, 1, 1]},
+                              {'mass': 4, 'pos': [1, 2, 3], 'vel': [2, -1, 2]}]
+    d = u.data()
+    assert d['particles'] == expected
